d2a45b211664afa208769baa8887db788b481179
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-01-31 22:44:58 +0400

    add content to website

---
+++ assign1/scripts/index.js
@@ -0,0 +1,12 @@
+// const main = () => {
+//     const blockOne = document.querySelector("#blockOne")
+//     // const videoContainer = document.querySelector("#videoContainer")
+//     const videoElement = document.querySelector("#video")
+//     const videoElementPadding = (blockOne.clientHeight - videoElement.clientHeight)/2
+//     videoElement.setAttribute("style", `margin-top:${videoElementPadding}px`)
+        
+// }
+
+// window.resize = main
+
+// main()
