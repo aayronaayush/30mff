150790497f19474c46d9a1eacaeaa24fde3a8559
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-02-10 00:27:40 +0400

    add commemts to script

--- scripts/crew.js
+++ scripts/crew.js
@@ -1,15 +1,19 @@
+// get DOM element for each crew member
 const aayush = document.getElementById("aayush")
 const alia = document.getElementById("alia")
 const ali = document.getElementById("ali")
 
+// add add event listener to handle the case for when mouse is over the photo
 aayush.addEventListener("mouseenter", () => handleMouseEnter(aayush))
 alia.addEventListener("mouseenter", () => handleMouseEnter(alia))
 ali.addEventListener("mouseenter", () => handleMouseEnter(ali))
 
+// add add event listener to handle the case for when mouse has left the photo
 aayush.addEventListener("mouseleave", () => handleMouseLeave(aayush))
 alia.addEventListener("mouseleave", () => handleMouseLeave(alia))
 ali.addEventListener("mouseleave", () => handleMouseLeave(ali))
 
+// show the crew information and make photo sligthly bigger
 const handleMouseEnter = (elem) => {
     const crewInfo = elem.getElementsByClassName("crewInfo")[0]
     const imageContainer = elem.getElementsByClassName("crewImage")[0]
@@ -17,6 +21,7 @@ const handleMouseEnter = (elem) => {
     crewInfo.style.display = "block"
 }
 
+// hide crew information and make photo normal size
 const handleMouseLeave = (elem) => {
     const crewInfo = elem.getElementsByClassName("crewInfo")[0]
     const imageContainer = elem.getElementsByClassName("crewImage")[0]
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,20 +1,23 @@
-// content blocks
+// get the content block elements
 const videoBlock = document.getElementById("videoBlock")
 const plotBlock = document.getElementById("plot")
 const crewBlock = document.getElementById("crew")
 const aboutBlock = document.getElementById("about")
 
-// nav bar items
+// get the nav bar button elements
 const videoNavButton = document.getElementById("videoNavButton")
 const plotNavButton = document.getElementById("plotNavButton")
 const crewNavButton = document.getElementById("crewNavButton")
 const aboutNavButton = document.getElementById("aboutNavButton")
 
+// initial display settings
 videoBlock.style.display = "block"
 plotBlock.style.display = "none"
 crewBlock.style.display = "none"
 aboutBlock.style.display = "none"
 
+// object of elements to aid operation on elems
+// keeps code DRY
 const pageElements = {
     video: {
         block: videoBlock,
@@ -34,16 +37,20 @@ const pageElements = {
     },
 }
 
+
+// get an array of the different content block
+// iterate through the array and add a click event listener
 Object.keys(pageElements).forEach(pageElement => {
     pageElements[pageElement].nav.addEventListener("click", () => contentBlockDisplay(pageElement) );
 })
 
+// change display style of display block
 const contentBlockDisplay = (target) => {
     console.clear()
     Object.keys(pageElements).forEach(element => {
-        if (element === target) {
+        if (element === target) { // the display style of "select" block to block ie make it visible
             pageElements[element].block.style.display = "block"
-        } else {
+        } else { // make the other block invisible or hidden
             pageElements[element].block.style.display = "none"
         }
     })
