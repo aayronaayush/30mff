9cf999d7ac4a531fa04be1ceb66ad9e18b25efe1
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-02-10 00:10:49 +0400

    remove gear section. add plot

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,20 +2,17 @@
 const videoBlock = document.getElementById("videoBlock")
 const plotBlock = document.getElementById("plot")
 const crewBlock = document.getElementById("crew")
-const gearBlock = document.getElementById("gear")
 const aboutBlock = document.getElementById("about")
 
 // nav bar items
 const videoNavButton = document.getElementById("videoNavButton")
 const plotNavButton = document.getElementById("plotNavButton")
 const crewNavButton = document.getElementById("crewNavButton")
-const gearNavButton = document.getElementById("gearNavButton")
 const aboutNavButton = document.getElementById("aboutNavButton")
 
 videoBlock.style.display = "block"
 plotBlock.style.display = "none"
 crewBlock.style.display = "none"
-gearBlock.style.display = "none"
 aboutBlock.style.display = "none"
 
 const pageElements = {
@@ -30,10 +27,6 @@ const pageElements = {
     crew: {
         block: crewBlock,
         nav: crewNavButton
-    },
-    gear: {
-        block: gearBlock,
-        nav: gearNavButton
     },
     about: {
         block: aboutBlock,
